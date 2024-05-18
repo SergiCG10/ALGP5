c44b58de0c4dbdb0296ce853340657e41d8554d7
Mangel <mangelluqueg@gmail.com>
2024-05-18 22:20:06 +0200

    Viva el Vino

--- problema2.cpp
+++ problema2.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 void ImprimeMatriz(vector<vector<int>> matrix);
 void RellenarMatriz(vector<vector<int>> matrix);
-static const int RETRASO=1;
+static const int ESCALA=1;
 
 int main(int argc, char* argv[])
 {
@@ -17,7 +17,7 @@ int main(int argc, char* argv[])
     
     int ciudades=atoi(argv[1]);
     int semilla=atoi(argv[2]);
-    char respuesta;
+    char respuesta='\0';
     vector<vector<int>> matrix;
     if(ciudades<4)
         ciudades=4;
@@ -26,10 +26,10 @@ int main(int argc, char* argv[])
     for(int i=0;i<ciudades;i++)
         matrix[i].resize(ciudades);
 
-    cout<<"¿Desea rellenar la matriz a mano? (escriba 'y' en caso afirmativo)"<<endl;
+    cout<<"¿Desea rellenar la matriz a mano? (escriba y en caso afirmativo)"<<endl;
     cin>>respuesta;
     
-    if(respuesta=!'y')
+    if(respuesta !='y')
     {
         cout<<"Se procede a inicializar con valores aleatorios"<<endl;
         for(int i=0;i<ciudades;i++){
@@ -52,7 +52,29 @@ int main(int argc, char* argv[])
         }    
     }
 
+    int partida,destino;
+    cout<<"\nSe muestran los vuelos disponibles a continuación."<<endl;
     ImprimeMatriz(matrix);
+    cout<<"Introduzca ciudad de partida (int)"<<endl;
+    cin>>partida;
+    while(partida<0 || partida>ciudades-1)
+    {
+        cout<<"Introduzca una ciudad en el rango [0,"<<ciudades-1<<"]"<<endl;
+        cin>>partida;
+    }
+    cout<<"Introduzca ciudad de destino (int)"<<endl;
+    cin>>destino;
+    while(destino<0 || destino>ciudades-1)
+    {
+        cout<<"Introduzca una ciudad en el rango [0,"<<ciudades-1<<"]"<<endl;
+        cin>>destino;
+    }
+
+    if(partida==destino)
+    {
+        cout<<"El viaje tiene una duracion de 0 horas, la partida y el origen coinciden"<<endl;
+        return 0;
+    }
     
     
     return 0;
@@ -60,7 +82,17 @@ int main(int argc, char* argv[])
 
 void ImprimeMatriz(vector<vector<int>> matrix)
 {
+    cout<<"    ";
+    for(int i=0;i<matrix.size();i++)
+        cout<<i+1<<" ";
+    cout<<endl;
+    cout<<"    ";
+
+    for(int i=0;i<matrix.size();i++)
+        cout<<"--";
+    cout<<endl;
     for(int i=0; i<matrix.size();i++){
+        cout<<i<<"|  ";
         for(int j=0;j<matrix.size();j++)
             cout<<matrix[i][j]<<" ";
         cout<<endl;
